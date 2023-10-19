ed14b4a3a0851fc1d9dcfa8a77b561bef925379b
Bradley Kukuk <brad@bdksoftware.com>
2023-10-19 17:21:15 -0600

    adding responsiveness to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
 import "../styles/Navbar.css";
+import { FaBars } from "react-icons/fa";
+
 
 export const Navbar = () => {
   const [navBar, setNavBar] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
 
-  function changeBackground(){
+  function changeBackground() {
     if (window.scrollY >= window.innerHeight - 100) {
       setNavBar(true);
     } else {
@@ -13,28 +16,41 @@ export const Navbar = () => {
   }
 
   useEffect(() => {
-    window.addEventListener('scroll', changeBackground);
+    window.addEventListener("scroll", changeBackground);
 
     return () => {
-      window.removeEventListener('scroll', changeBackground);
-    }
-  }, [])
-
- 
+      window.removeEventListener("scroll", changeBackground);
+    };
+  }, []);
 
   return (
     <>
-    <div className={navBar == true ? "navContainer navActive" : "navContainer"}>
-      <div className='navLogoContainer'>
-      <img src={"logo.png"} alt="KookCodes Logo" loading="eager"/>
+      <div
+        className={navBar == true ? "navContainer navActive" : "navContainer"}
+      >
+        <div className="navLogoContainer">
+          <img src={"logo.png"} alt="KookCodes Logo" loading="eager" />
+        </div>
+        <div className="navLinks">
+          <a href="#Home">Home</a>
+          <a href="#About">About</a>
+          <a href="#Projects">Portfolio</a>
+          <a href="#Contact">Contact</a>
+        </div>
+        <div className="navIcon">
+          <button className="navButton" onClick={() => setShowMenu(!showMenu)}>
+            <FaBars size={20} color={"#ffffff"} />
+          </button>
+        </div>
       </div>
-      <div className='navLinks'>
-        <a href="#Home">Home</a>
-        <a href="#About">About</a>
-        <a href="#Projects">Portfolio</a>
-        <a href="#Contact">Contact</a>
-      </div>
-    </div>
+      {showMenu && (
+        <div className="navMobileLinks">
+          <a href="#Home">Home</a>
+          <a href="#About">About</a>
+          <a href="#Projects">Portfolio</a>
+          <a href="#Contact">Contact</a>
+        </div>
+      )}
     </>
   );
 };
